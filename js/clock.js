68c2bd2734fb6c92fbe4ee3a402098af3caf9ad4
YoungHa Kim <ytok1108@aol.com>
2021-11-18 01:44:19 +0900

    time bug fix.

--- js/clock.js
+++ js/clock.js
@@ -5,12 +5,12 @@ function getClock() {
     const date =  new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const KrDate = new Date().toLocaleString(undefined, { year: 'numeric', month: '2-digit', day: '2-digit', weekday: "long", hour: '2-digit', hour12: false, minute: '2-digit', second: '2-digit' });
-    const NyDate = new Date(date.getTime() - date.getTimezoneOffset() * -33303).toISOString().split("T")[0];
-    const NyDate1 = new Date(date.getTime() - date.getTimezoneOffset() * -33303).toISOString().split("T")[1];
+    const NyDate = new Date(date.getTime() - date.getTimezoneOffset() * -33334).toISOString().split("T")[0];
+    const NyDate1 = new Date(date.getTime() - date.getTimezoneOffset() * -33334).toISOString().split("T")[1];
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `한국 ${KrDate}`;
     Nyclock.innerText =  `NewYork  ${NyDate}  ⏰   ${NyDate1} (EST = GMT -5:00)`;
+    clock.innerText = `한국 ${KrDate}`;
 }
 
 getClock();
