98e4cfbbd25e25ac0a7dd16c0ec01968ea1d858b
YoungHa Kim <ytok1108@aol.com>
2021-11-18 00:16:31 +0900

    211118 RealTime final

--- js/clock.js
+++ js/clock.js
@@ -10,7 +10,7 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = `한국 ${KrDate}`;
-    Nyclock.innerText =  `NewYork(EST)  ${NyDate}  ⏰   ${NyDate1}`;
+    Nyclock.innerText =  `NewYork  ${NyDate}  ⏰   ${NyDate1} (EST)`;
 }
 
 getClock();
